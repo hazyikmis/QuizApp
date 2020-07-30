8301825d769c24379b0deae8a1300969ff551ee4
Halil Azyikmis <hazyikmis@yahoo.com>
2020-07-30 23:56:00 +0200

    Updates

---
+++ precache-manifest.2f3d80a42724c7aefce34c390578cad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20eae43091ace289391a6a42dc15248",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "7cb4fcc6653598c061b6",
+    "url": "/QuizApp/static/js/2.b5e93213.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuizApp/static/js/2.b5e93213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f4359b096f9e2d0a3b1",
+    "url": "/QuizApp/static/js/main.bdb4fe14.chunk.js"
+  },
+  {
+    "revision": "a7f8694b575bd5cc29bc",
+    "url": "/QuizApp/static/js/runtime-main.7d9ee554.js"
+  },
+  {
+    "revision": "b14204dbc84a601ce6429a4237b09437",
+    "url": "/QuizApp/static/media/jonas-denil.b14204db.jpg"
+  }
+]);
